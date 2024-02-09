5a2c7bfc896f185cedeb190721488bdd66fee1a0
Matthew Hamilton <matthew.hamilton2@unibo.it>
2024-02-09 16:07:29 +0100

    shorten coef functions

--- src/matlab/D12_coeffs.m
+++ src/matlab/D12_coeffs.m
@@ -1,28 +1,5 @@
-function [u12_c,u13_c,u14_c,u11_c,u10_c,u02_c,u22_c,u32_c,u23_c,u21_c,u01_c,u03_c] = D12_coeffs(R0y,h,D,nu)
-
-u12_c = (20 - (2*D - R0y*h)/(2*D + R0y*h)) ;
-
-u13_c = - 8 ;
-
-u14_c = 1 ;
-
-u11_c = - 8 ;
-
-u10_c = 1 ;
-
-u02_c = ((4*D + 4*D*nu)/(2*D + R0y*h) - 8) ;
-
-u22_c = - 8 ;
-
-u32_c = 1 ;
-
-u23_c = 2 ;
-
-u21_c = 2 ;
-
-u01_c = (2 - (2*D*nu)/(2*D + R0y*h)) ;
-
-u03_c = (2 - (2*D*nu)/(2*D + R0y*h)) ;
-
+function varargout = D12_coeffs(R0y,h,D,nu)
+    out = [(20 - (2*D - R0y*h)/(2*D + R0y*h)), - 8, 1, - 8, 1, ((4*D + 4*D*nu)/(2*D + R0y*h) - 8), - 8, 1, 2, 2, (2 - (2*D*nu)/(2*D + R0y*h)), (2 - (2*D*nu)/(2*D + R0y*h))];
+    varargout = num2cell(out);
 end
 
--- src/matlab/D22_coeffs.m
+++ src/matlab/D22_coeffs.m
@@ -1,31 +1,4 @@
-function [u10_c,u11_c,u21_c,u31_c,u02_c,u12_c,u22_c,u32_c,u42_c,u13_c,u23_c,u33_c,u24_c] = D22_coeffs
-
-u10_c = 1 ;
-
-u11_c = 2 ;
-
-u21_c = -8 ;
-
-u31_c = 2 ;
-
-u02_c = 1 ;
-
-u12_c = -8 ;
-
-u22_c = 20 ;
-
-u32_c = -8 ;
-
-u42_c = 1 ;
-
-u13_c = 2 ;
-
-u23_c = -8 ;
-
-u33_c = 2 ;
-
-u24_c = 1 ;
-
-
+function varargout = D22_coeffs
+    out = [1, 2, -8, 2, 1, -8, 20, -8, 1, 2, -8, 2, 1];    
+    varargout = num2cell(out);
 end
-
