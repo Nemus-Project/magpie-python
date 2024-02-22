13e0ed68113d9ac24b5b45b03a47fcc295def98d
Matthew Hamilton <matthew.hamilton2@unibo.it>
2024-02-22 17:30:23 +0100

    first pass bhmat function

--- src/py/D22_coeffs.py
+++ src/py/D22_coeffs.py
@@ -1,4 +1,2 @@
-function varargout = D22_coeffs
-    out = [1, 2, -8, 2, 1, -8, 20, -8, 1, 2, -8, 2, 1];    
-    varargout = num2cell(out);
-end
+def D22_coeffs():
+    return [1, 2, -8, 2, 1, -8, 20, -8, 1, 2, -8, 2, 1];
