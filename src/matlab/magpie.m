681b6a6d6e267b81ba5d5f42264787a45238c706
Michele <122601846+mdphys@users.noreply.github.com>
2024-03-17 18:05:02 +0000

    Update magpie.m
    Relaxed the requirement to supply the total number of modes. Nmodes can be left blank, in which case all available modes will be computed.

--- src/matlab/magpie.m
+++ src/matlab/magpie.m
@@ -1,4 +1,4 @@
-function [Om,Q,Nx,Ny,biHarm] = magpie(rho,E,nu,ldim,h,BCs,Nmodes,plot_type)
+function [Om,Q,Nx,Ny,biHarm,Dm] = magpie(rho,E,nu,ldim,h,BCs,Nm,plot_type)
 % MAGPIE What does this do?
 %   [Om,Q,Nx,Ny,biHarm] = MAGPIE (rho,E,nu,Lx,Ly,Lz,h,K0y,R0y,Kx0,Rx0,KLy,RLy,KxL,RxL,Nmodes)
 %   A function that returns:
@@ -7,19 +7,20 @@ function [Om,Q,Nx,Ny,biHarm] = magpie(rho,E,nu,ldim,h,BCs,Nmodes,plot_type)
 %           Nx      : Grid points along the x-axis
 %           Ny      : Grid points along the y-axis
 %           biHarm  : Biharmonic Matrix for the plate
+%           Dm      : the eigenvalues of the biharmonic matrix 
 %
 %   Arguments:
 %       rho          %-- density [kg/m^3]
 %       E            %-- Young's mod [Pa]
 %       nu           %-- poisson's ratio
-%       
+%
 %       3 element array  representing  x,yz dimensions of plate
 %       ldim = [Lx,  %-- length along x [m]
 %               Ly,  %-- length along y [m]
 %               Lz]  %-- thickness [m]
 %
 %       h            %-- grid spacing
-%       
+%
 %       2 column array of eleastic boundary constants around each edge of
 %       the plate.
 %
@@ -31,7 +32,7 @@ function [Om,Q,Nx,Ny,biHarm] = magpie(rho,E,nu,ldim,h,BCs,Nmodes,plot_type)
 %              KLy, RLy;
 %              KxL, RxL];
 %
-%       Nmodes      %-- number of modes to compute
+%       Nm      %-- number of modes to compute
 %
 %
 %       Example:
@@ -42,79 +43,83 @@ function [Om,Q,Nx,Ny,biHarm] = magpie(rho,E,nu,ldim,h,BCs,Nmodes,plot_type)
 %           E       = 1.01e+11 ;        %-- Young's mod [Pa]
 %           rho     = 8765 ;            %-- density [kg/m^3]
 %           nu      = 0.3 ;             %-- poisson's ratio
-%           Nmodes  = 16;               %-- number of modes to compute
-%           h       = sqrt(Lx*Ly)*0.01; %--           
+%           Nm  = 16;               %-- number of modes to compute
+%           h       = sqrt(Lx*Ly)*0.01; %--
 %           BCs = ones(4,2) * 1e15      %-- elastic constants around the edges
-%           
-%           [Om,Q,Nx,Ny,biHarm] = magpie(rho, E, nu, ldim, h, BCs, Nmodes,'none');
-    %% Validation        
-    validateattributes(rho,      {'double'}, {'nonempty'});
-    validateattributes(E,        {'double'}, {'nonempty'});
-    validateattributes(nu,       {'double'}, {'nonempty'});
-    validateattributes(ldim,     {'double'}, {'numel', 3});
-    validateattributes(h,        {'double'}, {'nonempty'});    
-    validateattributes(BCs,      {'double'}, {'size', [4,2]});
-    validateattributes(Nmodes,   {'numeric'}, {'integer','positive'});
-    validatestring(plot_type,["chladni","3D","none"]);
-              
-    %% Unpack array variables
-    pack_ldim = num2cell(ldim);
-    pack_BCs = num2cell(BCs);
-    [Lx, Ly, Lz] = pack_ldim{:};
-    [K0y, Kx0, KLy, KxL, R0y, Rx0, RLy, RxL] = pack_BCs{:};
+%
+%           [Om,Q,Nx,Ny,biHarm,Dm] = magpie(rho, E, nu, ldim, h, BCs, Nm,'none');
+%% Validation
+validateattributes(rho,      {'double'}, {'nonempty'});
+validateattributes(E,        {'double'}, {'nonempty'});
+validateattributes(nu,       {'double'}, {'nonempty'});
+validateattributes(ldim,     {'double'}, {'numel', 3});
+validateattributes(h,        {'double'}, {'nonempty'});
+validateattributes(BCs,      {'double'}, {'size', [4,2]});
+% validateattributes(Nmodes,   {'numeric'}, {'integer','positive'});
+validatestring(plot_type,["chladni","3D","none"]);
 
-    %%--- derived parameters (don't change here)    
-    D = E * Lz^3 / 12 / (1-nu^2);
-    Nx      = floor(Lx/h) ;
-    Ny      = floor(Ly/h) ;
-    %%----------------------------
-    %% Build BiHarmonic
-    biHarm = bhmat(BCs,[Nx Ny], h, Lz, E, nu);
-    
-    %% EIGENVALUES
+%% Unpack array variables
+pack_ldim = num2cell(ldim);
+pack_BCs = num2cell(BCs);
+[Lx, Ly, Lz] = pack_ldim{:};
+[K0y, Kx0, KLy, KxL, R0y, Rx0, RLy, RxL] = pack_BCs{:};
 
-    [Q,Dm] = eigs(biHarm,Nmodes,'smallestabs') ;
-    [~,indSort] = sort(diag((Dm))) ;
-    Q = Q(:,indSort) ;
+%%--- derived parameters (don't change here)
+D = E * Lz^3 / 12 / (1-nu^2);
+Nx      = floor(Lx/h) ;
+Ny      = floor(Ly/h) ;
+%%----------------------------
+%% Build BiHarmonic
+biHarm = bhmat(BCs,[Nx Ny], h, Lz, E, nu);
+
+%% EIGENVALUES
+
+Nmodes = (Nx+1)*(Ny+1) ;
+if Nm
+    Nmodes = Nm ;
+end
+[Q,Dm] = eigs(biHarm,Nmodes,'smallestabs') ;
+[~,indSort] = sort(diag((Dm))) ;
+Q = Q(:,indSort) ;
+
+Dm    = diag(Dm) ;
+Om    = sqrt(abs(Dm))*sqrt(D/rho/Lz) ;
+%freqs = Om/2/pi ;
 
 
-    freqs = sqrt(abs(diag(Dm)))*sqrt(D/rho/Lz)/2/pi ;
-    Om    = 2*pi*freqs ;
+switch plot_type
+    case 'chladni'
+        subs = ceil(sqrt(Nmodes));
 
+        colormap('copper') ;
+        cmp = colormap;
+        cmp = flipud(cmp);
+        colormap(cmp);
 
-    switch plot_type
-        case 'chladni'
-            subs = ceil(sqrt(Nmodes));
-            
-            colormap('copper') ;
-            cmp = colormap;
-            cmp = flipud(cmp);
-            colormap(cmp);    
-                
-            for m = 1 : Nmodes
-                mdShape = reshape(Q(:,m),[(Ny+1),(Nx+1)]) ;
-                subplot(subs,subs,m)
-                mesh(3e3*real(mdShape),(abs(mdShape)),'FaceColor','texturemap') ; 
-                view(2); 
-                axis equal; 
-                axis tight;        
-                axis off;
-                clim([0.00005 0.002]);        
-            end
+        for m = 1 : Nmodes
+            mdShape = reshape(Q(:,m),[(Ny+1),(Nx+1)]) ;
+            subplot(subs,subs,m)
+            mesh(3e3*real(mdShape),(abs(mdShape)),'FaceColor','texturemap') ;
+            view(2);
+            axis equal;
+            axis tight;
+            axis off;
+            clim([0.00005 0.002]);
+        end
 
-        case '3D'
-        
-            subs = ceil(sqrt(Nmodes));
-            colormap('parula') ;
-            xax = (0:Nx)*h ;
-            yax = (0:Ny)*h ;
-            [X,Y] = meshgrid(xax,yax) ;
+    case '3D'
 
-            for m = 1 : Nmodes
-                mdShape = reshape(Q(:,m),[(Ny+1),(Nx+1)]) ;
-                subplot(subs,subs,m)
-                mesh(X,Y,3000*(mdShape),(abs(mdShape)),'FaceColor','texturemap') ;
-            end 
-    end
-    
+        subs = ceil(sqrt(Nmodes));
+        colormap('parula') ;
+        xax = (0:Nx)*h ;
+        yax = (0:Ny)*h ;
+        [X,Y] = meshgrid(xax,yax) ;
+
+        for m = 1 : Nmodes
+            mdShape = reshape(Q(:,m),[(Ny+1),(Nx+1)]) ;
+            subplot(subs,subs,m)
+            mesh(X,Y,3000*(mdShape),(abs(mdShape)),'FaceColor','texturemap') ;
+        end
+end
+
 end
