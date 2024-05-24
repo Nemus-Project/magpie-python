63231b80f37e076cb8da4991d6d3281aff7aed82
Matthew Hamilton <matthew.hamilton2@unibo.it>
2024-05-24 10:58:44 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nemus-magpie',
+    version='0.1.0',
+    packages=['magpie'],
+    url='https://github.com/nemus-Project/magpie-python',
+    license='GNU General Public License v3.0',
+    author='Michele Ducceschi, Matthew Hamilton',
+    author_email='matthew.hamilton.2@unibo.it',
+    description='An open source framework for working with thin plates under generalized elastic boundary conditions'
+)
