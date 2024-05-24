53fc8c39e198e613af42dc52e0c1db71028070d7
Matthew Hamilton <matthew.hamilton2@unibo.it>
2024-05-24 11:09:55 +0200

    set requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,12 @@ setup(
     url='https://github.com/nemus-Project/magpie-python',
     license='GNU General Public License v3.0',
     author='NEMUS',
-    author_email='michele.ducceschi@unibo.it',
-    description='An open source framework for working with thin plates under generalized elastic boundary conditions'
+    author_email='michele.ducceschi@unibo.it    ',
+    description='An open source framework for working with thin plates under generalized elastic boundary conditions',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'sounddevice',
+    ]
 )
