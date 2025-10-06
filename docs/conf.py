8c87201216025be3ba6126032976de3a3ea1f559
Matthew Hamilton <matthew.hamilton2@unibo.it>
2025-10-06 17:13:21 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,12 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+#
+#
+
+# -- Build Documentation -----------------------------------------------------
+# In the same directory as this conf.py run: 
+# sphinx-build -M html ./ ./
 
 # -- Path setup --------------------------------------------------------------
 
